2686f3398e22a1291dc17bff6e7687e4a128ea2e
Zach Dennis <zach.dennis@gmail.com>
2015-09-04 15:49:50 -0400

    read secret key base from environment

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module InternetPoints
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
